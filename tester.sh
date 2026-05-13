d9c24a9bb48f29fede972840ee59be901e18807c
Joachim MASSON <joachim.masson.17@gmail.com>
2026-05-13 17:29:43 +0200

    fix bug with tester.sh (originaly for linux but I work on windows) and do PrimitiveTraining

--- tester.sh
+++ tester.sh
@@ -4,14 +4,19 @@ if [ -n "$1" ]; then
     classname="$1"
     classname="$(tr '[:lower:]' '[:upper:]' <<< ${classname:0:1})${classname:1}"
 
-    javac -classpath out:lib/junit-4.12.jar -d out src/exercices/${classname}.java 2> logs/checkmethod_output.txt
+    # Utilisation du point-virgule pour Windows et ajout de hamcrest pour éviter les erreurs junit
+    CP="out;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar"
+
+    javac -classpath "$CP" -d out src/exercices/${classname}.java 2> logs/checkmethod_output.txt
     if [[ $(< logs/checkmethod_output.txt) != "" ]]; then
         cat logs/checkmethod_output.txt
     else
-        javac -classpath out:lib/junit-4.12.jar -d out src/tests/MainTest.java
-        javac -classpath out:lib/junit-4.12.jar -d out src/tests/${classname}Test.java
+    # Compilation des tests avec le bon classpath
+        javac -classpath "$CP" -d out src/tests/MainTest.java
+        javac -classpath "$CP" -d out src/tests/${classname}Test.java
 
-        java -classpath out:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore ${classname}Test
+        # Exécution du test (on ajoute 'out' pour que Java trouve les fichiers .class compilés)
+        java -classpath "$CP;out" org.junit.runner.JUnitCore ${classname}Test
     fi
 else
     echo "Please specify the class to test, e.g. : ./tester.sh PrimitiveTraining"
